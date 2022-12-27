cc69e7d1233aad41b9521fe9fa591015e5d7b1da
Vu Phan <phananhvuk5@gmail.com>
2022-12-27 15:12:21 +0700

    Save code changes for iOS

--- ios/Runner/MyRender.h
+++ ios/Runner/MyRender.h
@@ -1,28 +1,29 @@
-////
-////  MyRender.h
-////  Runner
-////
-////  Created by Vu Phan on 12/9/22.
-////
 //
-//#import <Foundation/Foundation.h>
-//#import <UnityFramework/NativeCallProxy.h>
-//#import <UIKit/UIKit.h>
+//  MyRender.h
+//  Runner
 //
+//  Created by Vu Phan on 12/9/22.
 //
-//
-//NS_ASSUME_NONNULL_BEGIN
-//
-//@interface MyRender : NSObject <NativeCallsProtocol>
-//@property NSString* oldTextureId;
-//@property (nonatomic, retain)NSData *data;
-//@property id<MTLTexture> myTexture;
-//
-//    - (void) showHostMainWindow;
-//    - (void) unloadPlayer;
-//    - (void) quitPlayer;
-//    - (void) sendMessageToMobileApp:(char *) mtlTexture
-//                          textureId:(NSString *) unityTextureId;
-//
-//@end
-//NS_ASSUME_NONNULL_END
+
+#import <Foundation/Foundation.h>
+#import <UnityFramework/NativeCallProxy.h>
+#import <UIKit/UIKit.h>
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyRender : NSObject <NativeCallsProtocol>
+@property NSString* oldTextureId;
+@property NSData* data;
+@property Boolean isDraw;
+@property id<MTLTexture> myTexture;
+
+    - (void) showHostMainWindow;
+    - (void) unloadPlayer;
+    - (void) quitPlayer;
+    - (void) sendMessageToMobileApp:(char *) mtlTexture
+                          textureId:(NSString *) unityTextureId;
+
+@end
+NS_ASSUME_NONNULL_END
